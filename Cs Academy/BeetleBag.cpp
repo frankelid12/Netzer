5d5a80041a20044ed8fe2dca7a0289cf06700aa7
shiranka <42327146+shiranka@users.noreply.github.com>
2019-08-21 13:01:33 +0300

    c++

--- Cs Academy/BeetleBag.cpp
+++ Cs Academy/BeetleBag.cpp
@@ -1,24 +1,34 @@
-    
 #include <bits/stdc++.h>
 using namespace std;
-const int N = 500;
+
+
+const int N = 500; //כמות הקלטים המקסימלית
+//מערך גךלובלי למשקולות, מערך ל כוח הליחמה ומערך למציאת המקסימלים
 int w[N], f[N], arr[N];
+
 int main() {
-  int num_of_test_cases;
-  scanf("%d", &num_of_test_cases);
-  while (num_of_test_cases--) {
-    memset(arr, 0, sizeof(arr));
-    int c,n;
-    scanf("%d %d", &c, &n);
-    for (int i = 0; i < n; i++) scanf("%d %d", w + i, f + i);
+  int t; //מספר מקרים
+  cin>>t; //קליטת מספר מקרים
+  //לולאה שעוברת על כמות המקרים וקולטת ומחשבת
+  while (t--) {
+    memset(arr, 0, sizeof(arr)); //מאפסת את המערך
+    int c,n; //capacity and gadgets
+    cin>>c>>n; //קליטה של הערכים
+    //קליטה של הכמות של זוגות שצריך
+    for (int i = 0; i < n; i++) {
+        cin>>w[i]>>f[i];//קליטת מקבל וכוח לחימה למערכים
+    }
     for (int i = 0; i < n; i++) {
       for (int j = c; j >= w[i]; j--) {
-        arr[j] = max(arr[j], arr[j-w[i]] + f[i]);
+        arr[j] = max(arr[j], arr[j-w[i]] + f[i]);//חישוב הערך המקסימלי והכנסתו למערך
       }
     }
     int sol = 0;
-    for (int i = 0; i <= c; i++) sol = max(sol, arr[i]);
-    cout << sol << endl;
+    for (int i = 0; i <= c; i++){
+        sol = max(sol, arr[i]); // מציאת המקסימאלי לאחר השקלולים
+    }
+    cout << sol << "\n";//הדפסת הערך המקסימלי
   }
+     
   return 0;
 }
