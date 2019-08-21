3077a1831998bd7298b92aa07eb3abe13e121b28
frankelid12 <45718604+frankelid12@users.noreply.github.com>
2019-08-21 15:32:57 +0300

    Update Rumour.cpp

--- Cs Academy/Rumour.cpp
+++ Cs Academy/Rumour.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <cstdio>
 #include <algorithm>
 
@@ -7,33 +8,39 @@ typedef unsigned long long llu;
 
 int main( void ) {
     int Q;
-    scanf("%d", &Q );
+    cin>>Q;
     while( Q-- ) {
-        llu a, b;
-        scanf("%llu%llu", &a, &b );
-        int sol = 0;
-        llu at = a, bt = b;
-        int de1 = 0, de2 = 0;
-        while( at > 0 ) {
-            de1++;
-            at /= 2LLU;
+        llu u, v;
+        cin>>u>>v;
+        int ans = 0;
+        llu uu = u, vv = v;
+        int depth1 = 0, depth2 = 0;
+        while( uu > 0 ) {
+            depth1++;
+            //every time i going up level
+            uu /= 2LLU;
         }
-        while( bt > 0 ) {
-            de2++;
-            bt /= 2LLU;
+        while( vv > 0 ) {
+            
+            depth2++;
+            vv /= 2LLU;
         }
-        if( de1 > de2 ) {
-            swap( a, b );
-            swap( de1, de2 );
+        if( depth1 > depth2 ) {
+            swap( u, v );
+            swap( depth1, depth2 );
         }
-        b /= ( llu )( 1 << ( llu )( de2 - de1 ) );
-        sol += de2 - de1;
-        while( a != b ) {
-            a /= 2LLU;
-            b /= 2LLU;
-            sol += 2;
+        //lift the deapest node to the same level as the other one
+        v /= ( llu )( 1 << ( llu )( depth2 - depth1 ) );
+        //add the sub to the answer
+        ans += depth2 - depth1;
+        //until they wont meet in the same node the loop won't stop 
+        while( u != v ) {
+            u /= 2LLU;
+            v /= 2LLU;
+            //every loop increase the dis in 2 and going up with the nodes
+            ans += 2;
         }
-        printf("%d\n", sol);
+        printf("%d\n", ans);
     }
     return 0;
 }
